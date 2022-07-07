1169791e18ba76b5d802f1ad85a5f099db3b0f12
qiushaocloud <qiushaocloud@126.com>
2022-07-07 10:45:24 +0800

    update *.sh

--- run-docker.sh
+++ run-docker.sh
@@ -5,4 +5,15 @@ if [ ! -f "$CURR_DIR/.env" ];then
   exit
 fi
 
+REDIS_DATA_PATH=`grep REDIS_DATA_PATH $CURR_DIR/.env|awk -F '=' '{print $2}' | awk '$1=$1' | sed -e "s/'//g"`
+REDIS_CFG_FILE_PATH=`grep REDIS_CFG_FILE_PATH $CURR_DIR/.env|awk -F '=' '{print $2}' | awk '$1=$1' | sed -e "s/'//g"`
+
+mkdir -p $REDIS_DATA_PATH
+
+if [ ! -f "$REDIS_CFG_FILE_PATH" ];then
+  echo "file $REDIS_CFG_FILE_PATH is not exist, need copy $CURR_DIR/test-volumes/redis.conf to $REDIS_CFG_FILE_PATH"
+  mkdir -p `dirname $REDIS_CFG_FILE_PATH`
+  cp $CURR_DIR/test-volumes/redis.conf $REDIS_CFG_FILE_PATH
+fi
+
 docker-compose up -d
